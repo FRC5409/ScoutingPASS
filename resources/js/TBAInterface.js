554563074c52e98814ab08501033a05b8cb84240
Chris Brooks-Prenger <brooksprench@hdsb.ca>
2023-02-25 13:56:38 -0500

    Update TBAInterface.js

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "DGoZaWu5a9N5Ivs5a79fVUcd5NPld7QNM7LTjHMq4UgRdFVDSZmbxWBrZaTbqEKB";
 /**
  * Get list of teams in event
  *
