57f82c49540efbfea872f2b0e856685da688dff9
Yonina W <100385243+yoninaW4917@users.noreply.github.com>
2024-03-27 22:08:11 -0300

    updated event code for mcmaster

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -2,7 +2,7 @@ var config_data = `
 {
   "dataFormat": "tsv",
   "title": "Scouting PASS 2024 - Not",
-  "page_title": "Crescendo - North Bay",
+  "page_title": "Crescendo - McMaster",
   "checkboxAs": "10",
   "prematch": [
     { "name": "Scouter Initials",
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2024onnob",
+      "defaultValue": "2024onham",
       "required": "true"
     },
     { "name": "Match Level",
