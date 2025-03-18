694f3530c6e0ca010126e3c1b1788f71ee3c6d92
Yonina W. <100385243+yoninaW4917@users.noreply.github.com>
2025-03-18 15:41:12 -0400

    update event to north bay

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2025onnew",
+      "defaultValue": "2025onnob",
       "required": "true"
     },
     { "name": "Match Level",
